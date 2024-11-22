b3a61c919cb3d58cbef56f7b1de8da42d44f3688
ShotgunApe <shotgunayyype@gmail.com>
2024-11-21 21:16:24 -0500

    cleaning up

--- code/AudioPlugin/DelayLine.h
+++ code/AudioPlugin/DelayLine.h
@@ -11,12 +11,11 @@ public:
     void fillBuffer (juce::AudioBuffer<float>& buffer, int channel);
     void readFromBuffer (juce::AudioBuffer<float>& buffer, juce::AudioBuffer<float>& delayBuffer, int channel);
     void updateBufferPosition (juce::AudioBuffer<float>& buffer, juce::AudioBuffer<float>& delayBuffer);
-    void prepare ();
     
     juce::AudioBuffer<float>& get ();
 
 private:
     // For circular buffer
-    juce::AudioBuffer<float> delayBuffer;
+    juce::AudioBuffer<float> delayBuffer; 
     int writePosition { 0 };
 };
--- code/AudioPlugin/PluginEditor.cpp
+++ code/AudioPlugin/PluginEditor.cpp
@@ -36,7 +36,7 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    // g.drawFittedText ("Hello World again!", getLocalBounds(), juce::Justification::centred, 1);
+   // g.drawFittedText ("Hello World again!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
