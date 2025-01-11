ef4c7b1f24de6195fc592a058cc11efaf1d81bc0
ShotgunApe <shotgunayyype@gmail.com>
2025-01-10 23:04:18 -0500

    smol change

--- code/AudioPlugin/PluginProcessor.cpp
+++ code/AudioPlugin/PluginProcessor.cpp
@@ -137,9 +137,7 @@ void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     }
     
     for (int channel = 0; channel < totalNumInputChannels; ++channel) {
-
-        // step 1 - Diffuse Initial Impact Response
-        // step 2 - Delay (Network ... eventually)
+        // Delay (Network ... eventually)
         reverb.processReverb(buffer, channel);
     }
     reverb.updatePosition(buffer);
