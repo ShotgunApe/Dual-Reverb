94b7c8dfd73493403d7d34992580e5ced4fe569c
ShotgunApe <shotgunayyype@gmail.com>
2025-01-16 12:49:13 -0500

    small changes, added TODO

--- code/AudioPlugin/TestReverb.cpp
+++ code/AudioPlugin/TestReverb.cpp
@@ -16,10 +16,13 @@ void TestReverb::prepareReverb(double sampleRate)
     channelA.getBuffer().clear();
     channelA.getBuffer().setSize (2, (int) delayBufferSize); // output is STEREO (this caused me so much pain)
 
-    // create random amount of delay length for delay line :)
+    // create random amount of delay length for delay line (for now)
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist6(4800, 9600); // buffer duration 100ms - 200ms (for 48000hz sample rate, need to fix any any sample rate)
+    
+    // TODO: have it so that delaytime + gain are the same REGARDLESS of delay length
+    // also maybe pass parameter to delayline so that delaytimes can be set when delaylines are initialized?
     channelA.setDelay(dist6(rng));
 }
 
